3f18598c053ddf87226defe5019c7ddd87445296
Mohit <mohitramola98@gmail.com>
2021-08-16 21:13:49 +0530

    added button

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 import nav from "../Assests/nav.svg";
+import { BsFillPlayFill } from "react-icons/bs";
 const Hero = () => {
   return (
     <section className="bg  p-4  text-sm-start">
@@ -8,8 +9,8 @@ const Hero = () => {
         <div className="d-md-flex">
           <div>
             <h1 className="tag-line">Best detination around the world</h1>
-            <p className="moto  ">Travel,enjoy and live a new</p>
-            <p className="hero-discription py-3">
+            <p className="moto">Travel,enjoy and live a new</p>
+            <p className="hero-discription py-3 ">
               Built Wicket longer admire do barton vanity itself do in it.
               Preferred to sportsmen it engrossed listening. Park gate sell they
               west hard for the.
@@ -17,6 +18,10 @@ const Hero = () => {
             <div classname="d-flex">
               <button type="button" className="hero-button ">
                 Find Out More
+              </button>
+              <button type="button" className="play-button">
+                <BsFillPlayFill className="play-icon  play-circle " />
+                Play Demo
               </button>
             </div>
           </div>
